21279a2ac2d95176396296ce40ed00e92c8534e3
Keerthana <Keerthu98@users.noreply.github.com>
2019-03-21 01:33:16 +0530

    Version 2 first commit

---
+++ app/src/test/java/com/example/vicky/e_coin/welcomeTest.java
@@ -0,0 +1,24 @@
+package com.example.vicky.e_coin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class welcomeTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onRestart() {
+    }
+
+    @Test
+    public void initCipher() {
+    }
+
+    @Test
+    public void intent1() {
+    }
+}
